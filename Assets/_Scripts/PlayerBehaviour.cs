e02c5a81cf7f65a8f60cf68b61114b964e708e31
Ricky <nguyenkhanglhp1605@gmail.com>
2021-03-14 00:11:57 -0400

    added jump, pause, map button

--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -70,21 +70,33 @@ public class PlayerBehaviour : MonoBehaviour
 
         controller.Move(move * maxSpeed * Time.deltaTime);
 
-        if (Input.GetButton("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravity);
-            jumpSound.Play();
-        }
+        // Keyboard control
+        //if (Input.GetButton("Jump") && isGrounded)
+        //{
+        //    Jump();
+        //}
 
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
 
-        if (Input.GetKeyDown(KeyCode.M))
-        {
-            // toggle the MiniMap on/off
-            miniMap.SetActive(!miniMap.activeInHierarchy);
-        }
+        // Keyboard control
+        //if (Input.GetKeyDown(KeyCode.M))
+        //{
+        //    ToggleMinimap();
+        //}
+    }
+
+    void Jump()
+    {
+        velocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravity);
+        jumpSound.Play();
+    }
+
+    void ToggleMinimap()
+    {
+        // toggle the MiniMap on/off
+        miniMap.SetActive(!miniMap.activeInHierarchy);
     }
 
     void OnDrawGizmos()
@@ -104,5 +116,16 @@ public class PlayerBehaviour : MonoBehaviour
         }
     }
 
-   
+    public void OnJumpButtonPressed()
+    {
+        if(isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    public void OnMapButtonPressed()
+    {
+        ToggleMinimap();
+    }
 }
